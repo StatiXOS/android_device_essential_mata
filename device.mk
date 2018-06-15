56abe8f6b34cefb15964977e638e489645f76ec6
celtare21 <celtare21@gmail.com>
2018-06-15 00:28:11 +0000

    Revert "mata: Swap perf stack for wahoo's"
    This reverts commit dfe05609245c60f91251efe90a9996521d8ceebb.

--- device.mk
+++ device.mk
@@ -325,6 +325,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
+PRODUCT_COPY_FILES += \
+    device/essential/mata/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QCOM
 PRODUCT_COPY_FILES += \
     device/essential/mata/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
