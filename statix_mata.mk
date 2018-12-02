abe6544ee02628f870b26197cc12139a0b643686
Anay Wadhera <anay1018@gmail.com>
2018-12-02 15:55:16 -0800

    mata: inherit gsm parts

--- statix_mata.mk
+++ statix_mata.mk
@@ -1,6 +1,7 @@
 # Inherit some common statix stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
