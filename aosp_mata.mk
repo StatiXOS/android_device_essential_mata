9210233a82bbf50d9aae2fc57b5bcdb59ea6958d
Anay Wadhera <anay1018@gmail.com>
2019-01-05 20:13:28 -0600

    mata: aosp-ify

--- aosp_mata.mk
+++ aosp_mata.mk
@@ -1,7 +1,5 @@
-# Inherit some common Lineage stuff.
+# Inherit some common stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
@@ -20,7 +18,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := lineage_mata
+PRODUCT_NAME := aosp_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
