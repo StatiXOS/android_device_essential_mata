eea9c13386d22b56cca6f4e5cd4de8ddfadeaea9
Anay Wadhera <anay1018@gmail.com>
2019-04-12 19:02:39 -0700

    mata: update config.fs from CAF device/qcom/common
    revision: LA.UM.7.2.c2-01300-8x98.0
    
    Change-Id: I3f219f68b8b7f3a572e52895f6b7b34b1892513a

--- config.fs
+++ config.fs
@@ -1,6 +1,21 @@
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
+[AID_VENDOR_QDSS]
+value:2902
+
+[AID_VENDOR_RFS]
+value:2903
+
+[AID_VENDOR_RFS_SHARED]
+value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
+
 [vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
@@ -136,6 +151,18 @@ caps: NET_BIND_SERVICE BLOCK_SUSPEND
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
 
