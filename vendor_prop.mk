7639792d05b223e7a6d219cd924345659cf2165e
Jake Weinstein <jake@aospa.co>
2019-08-14 21:58:47 +0200

    mata: Match CAF display configuration

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -55,7 +55,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.qualcomm.cabl=2 \
     ro.sf.lcd_density=480 \
-    vendor.display.disable_partial_split=1
+    vendor.display.disable_partial_split=1 \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.perf_hint_window=50 \
+
 
 # Enforce privapp-permissions whitelist
 PRODUCT_PROPERTY_OVERRIDES += \
