666317ec92609824f1d2db3417aaabea1c261b65
Andrew Ozahoski <aozahoski@gmail.com>
2019-09-08 15:28:28 -0400

    mata: potato

--- potato_mata.mk
+++ potato_mata.mk
@@ -1,7 +1,6 @@
-# Inherit some common statix stuff.
+# Inherit some common potato stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/statix/config/common.mk)
-$(call inherit-product, vendor/statix/config/gsm.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -21,7 +20,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := statix_mata
+PRODUCT_NAME := potato_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
