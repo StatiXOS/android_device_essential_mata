17f1d656d33bfb2ea7706c186e64ebe34ef90635
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-09 23:26:32 +0300

    mata: Add packages for Android 10 IMS
    Change-Id: Ib387f4abcfc1895a420e05c4c87e7d00e65f02ba
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- device.mk
+++ device.mk
@@ -178,7 +178,8 @@ PRODUCT_COPY_FILES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common
+    ims-ext-common \
+    ims_ext_common.xml
 
 # Init
 PRODUCT_PACKAGES += \
@@ -349,7 +350,12 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    qti-telephony-common
+    telephony-ext \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \
