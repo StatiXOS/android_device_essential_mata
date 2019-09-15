639606263d0d38ddd931e160f66c2fc38cadfc59
Anay Wadhera <anay1018@gmail.com>
2019-09-15 11:38:19 -0700

    Revert "mata: potato"
    This reverts commit 666317ec92609824f1d2db3417aaabea1c261b65.

--- statix_mata.mk
+++ statix_mata.mk
@@ -1,6 +1,7 @@
-# Inherit some common potato stuff.
+# Inherit some common statix stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -26,7 +27,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := potato_mata
+PRODUCT_NAME := statix_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
