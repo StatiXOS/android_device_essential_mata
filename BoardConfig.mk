d1e130db7a877902ad4f67f84968c2b4e7a5bc69
Andrew Thomas <aozahoski@gmail.com>
2019-10-28 16:28:14 -0400

    Revert "mata: sepolicy"
    This reverts commit 86beb3762b776563ce6f32539c8f7c51813db157.
    
    Change-Id: I0ca69f67dd99316b8b25c121f75fb3365f25ebf8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -210,7 +210,6 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/temp
 
 # Timeservice
 BOARD_USES_QC_TIME_SERVICES := true
