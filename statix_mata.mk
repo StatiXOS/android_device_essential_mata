c0f4be4510778cbebfab959769d3a0e53d050fe2
Anay Wadhera <awadhera@berkeley.edu>
2019-12-25 01:28:28 -0800

    mata: statix-ify

--- statix_mata.mk
+++ statix_mata.mk
@@ -1,42 +1,38 @@
-# Inherit some common Lineage stuff.
+# Inherit some common statix stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/essential/mata/mata-vendor.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Device
 $(call inherit-product, device/essential/mata/device.mk)
 
-# TWRP
-ifeq ($(WITH_TWRP),true)
-$(call inherit-product, device/essential/mata/twrp/twrp.mk)
-else
 TARGET_RECOVERY_FSTAB := device/essential/mata/rootdir/etc/fstab.mata
-endif
 
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := lineage_mata
+PRODUCT_NAME := statix_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
 PRODUCT_RELEASE_NAME := mata
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRODUCT_NAME=mata \
+        PRIVATE_BUILD_DESC="mata-user 8.1.0 OPM1.180104.092 224 release-keys"
+
+BUILD_FINGERPRINT := essential/mata/mata:8.1.0/OPM1.180104.092/224:user/release-keys
+
 PRODUCT_ACTIONABLE_COMPATIBLE_PROPERTY_DISABLE := true
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 PRODUCT_CHARACTERISTICS := nosdcard
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRODUCT_NAME=mata \
-        PRIVATE_BUILD_DESC="mata-user 10 QP1A.190711.148 446 release-keys"
-
-BUILD_FINGERPRINT := essential/mata/mata:10/QP1A.190711.148/446:user/release-keys
