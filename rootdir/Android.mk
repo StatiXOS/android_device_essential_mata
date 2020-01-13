29d9d84def28be311ec6e3b19e85890b5033ae9a
Anay Wadhera <awadhera@berkeley.edu>
2020-01-12 17:42:24 -0800

    mata: overlay init scripts
    Change-Id: I4ab16b343039ae77689795fb10d932979496e174

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -8,7 +8,7 @@ LOCAL_MODULE       := fstab.mata
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mata
-LOCAL_VENDOR_MODULE    := true
+LOCAL_VENDOR_OVERLAY_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -16,7 +16,7 @@ LOCAL_MODULE       := fstab.persist
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.persist
-LOCAL_VENDOR_MODULE    := true
+LOCAL_VENDOR_OVERLAY_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -24,7 +24,7 @@ LOCAL_MODULE       := init.mata.ramdump.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mata.ramdump.rc
-LOCAL_VENDOR_MODULE    := true
+LOCAL_VENDOR_OVERLAY_MODULE    := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -33,7 +33,7 @@ LOCAL_MODULE       := init.mata.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mata.rc
-LOCAL_VENDOR_MODULE    := true
+LOCAL_VENDOR_OVERLAY_MODULE    := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -42,7 +42,7 @@ LOCAL_MODULE       := init.mata.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.mata.usb.rc
-LOCAL_VENDOR_MODULE    := true
+LOCAL_VENDOR_OVERLAY_MODULE    := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -51,7 +51,7 @@ LOCAL_MODULE       := init.qcom.devstart.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := bin/init.qcom.devstart.sh
-LOCAL_VENDOR_MODULE    := true
+LOCAL_VENDOR_OVERLAY_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -59,7 +59,7 @@ LOCAL_MODULE       := init.qcom.ipastart.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := bin/init.qcom.ipastart.sh
-LOCAL_VENDOR_MODULE    := true
+LOCAL_VENDOR_OVERLAY_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -67,7 +67,7 @@ LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := bin/init.qcom.post_boot.sh
-LOCAL_VENDOR_MODULE    := true
+LOCAL_VENDOR_OVERLAY_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -77,5 +77,5 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.mata.rc
 # this needs to be in /vendor/ueventd.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_OVERLAY)
 include $(BUILD_PREBUILT)
