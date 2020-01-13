201bb4020251f04881b2387e4bf08ef3203d4690
Anay Wadhera <awadhera@berkeley.edu>
2020-01-12 23:47:31 -0800

    mata: drop unneeded audio configs
    Change-Id: I3f459e27d8b8758754956e42f6ed33ca1a6f6e94

--- device.mk
+++ device.mk
@@ -55,21 +55,10 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    device/essential/mata/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/audio_effects.xml \
-    device/essential/mata/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/audio_platform_info.xml \
-    device/essential/mata/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/audio_policy_configuration.xml \
-    device/essential/mata/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/audio_policy_volumes.xml \
-    device/essential/mata/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/default_volume_tables.xml \
     device/essential/mata/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/listen_platform_info.xml \
-    device/essential/mata/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/mixer_paths_tasha.xml \
-    device/essential/mata/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/sound_trigger_mixer_paths.xml \
-    device/essential/mata/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/hearing_aid_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR_OVERLAY)/etc/usb_audio_policy_configuration.xml
 
 # A/B
 AB_OTA_UPDATER := true
