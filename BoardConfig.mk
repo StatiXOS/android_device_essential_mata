8b69190d1a8e73fca7aecb6a14b4108324e76663
Anay Wadhera <awadhera@berkeley.edu>
2020-01-13 14:33:13 -0800

    mata: drop vendor policy
    Change-Id: Ib93edef13e43e54532e3ff3fe4c1726b796b2872

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,7 +185,6 @@ BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 
