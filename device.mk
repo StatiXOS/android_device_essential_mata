d57200e96df66770ba27e56aea83fb66cca68ddd
Anay Wadhera <awadhera@berkeley.edu>
2020-01-12 23:48:34 -0800

    mata: drop seccomp
    Change-Id: I990b0483e89ab79bb1a2af8b03396a50a7535f73

--- device.mk
+++ device.mk
@@ -277,11 +277,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-# Seccomp
-PRODUCT_COPY_FILES += \
-    device/essential/mata/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    device/essential/mata/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
