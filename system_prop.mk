c26218451aafd2c749164753f71a67771a83dfe6
Anay Wadhera <awadhera@berkeley.edu>
2020-01-14 09:06:53 -0800

    mata: shuffle props
    Change-Id: Ieba118aa690bae07ffa153a5e158f877ddff297c

--- system_prop.mk
+++ system_prop.mk
@@ -20,6 +20,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
+# Display
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    vendor.display.disable_partial_split=1 \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.display.disable_skip_validate=1 \
+    vendor.display.perf_hint_window=50 \
+
+# Surfaceflinger
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.surface_flinger.protected_contents=true \
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000
 
 # Voice assistant
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
