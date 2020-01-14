cd968713b6d9601ca09f67bbb596463f29f4cc2b
Anay Wadhera <awadhera@berkeley.edu>
2020-01-13 22:24:04 -0800

    mata: use stock USB init scripts
    Change-Id: I8e1a8829f92b0a7cc17c0008ae37e4aac95e1bc6

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -38,15 +38,6 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mata.usb.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mata.usb.rc
-LOCAL_VENDOR_OVERLAY_MODULE    := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.devstart.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
