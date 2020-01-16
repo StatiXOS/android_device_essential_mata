cec047da73aee056fd430303c2dbe1b6cf692e60
Anay Wadhera <awadhera@berkeley.edu>
2020-01-15 21:46:31 -0800

    mata: update light service to be overlaid properly
    Change-Id: If1d6e35968b97e574dbd22df0dd60927f6456311

--- light/Android.mk
+++ light/Android.mk
@@ -2,10 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.light@2.0-service.mata
+LOCAL_MODULE_STEM := android.hardware.light@2.0-service
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_OVERLAY_MODULE := true
-LOCAL_INIT_RC := android.hardware.light@2.0-service.mata.rc
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
