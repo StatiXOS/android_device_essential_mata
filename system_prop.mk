65861b797a3eab716bf9132615cca8b87365d9ec
Anay Wadhera <awadhera@berkeley.edu>
2020-01-22 12:38:10 -0800

    mata: inherit the proper 4gb props from frameworks/native
    Change-Id: Ieebb9d4af45ffcf18d50448d88fe73dc2ee5a37f

--- system_prop.mk
+++ system_prop.mk
@@ -15,15 +15,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vidc.enc.dcvs.extra-buff-count=2 \
     vendor.camera.aux.packagelist="org.codeaurora.snapcam,com.essential.klik,org.lineageos.snap"
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
-
 # Display
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.display.disable_partial_split=1 \
