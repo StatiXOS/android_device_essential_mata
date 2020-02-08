1039254c9d6bbc2088e9360864ef223be30a4984
Anay Wadhera <awadhera@berkeley.edu>
2020-02-08 12:27:29 -0800

    mata: build more telephony packages
    Change-Id: I0cff203c36b296dbbb14c2d7a239b79c96399f53

--- device.mk
+++ device.mk
@@ -170,7 +170,13 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    telephony-ext
+    telephony-ext \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
