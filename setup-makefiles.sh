87b596ae2f08e65bfb2937cb7c1dc6bd981d6baa
Alexander Koskovich <zvnexus@outlook.com>
2020-04-28 16:08:12 -0400

    mata: Update prop list.
    Change-Id: Ib9bbff5b8c428a2b3f391998a148f64810b50766

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -27,9 +27,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$ROOT"/vendor/statix/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -37,20 +37,14 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$ROOT"
 
 # Copyright headers and guards
 write_headers
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 
-cat << EOF >> "$BOARDMK"
-TARGET_RECOVERY_DEVICE_DIRS += vendor/$VENDOR/$DEVICE/proprietary
-EOF
-
-cat << EOF >> "$ANDROIDMK"
-
-EOF
+sed -i 's/TARGET_COPY_OUT_VENDOR/TARGET_COPY_OUT_VENDOR_OVERLAY/g' "$PRODUCTMK"
 
 # Finish
 write_footers
