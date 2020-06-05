174fd444a6ce287286bf9d71d41c3559f77dc166
Alexander Koskovich <zvnexus@outlook.com>
2020-06-05 02:23:33 +0000

    mata: Cleanup DT.
    Change-Id: Iceae379e336184c7fcdec86ff76b751027460873

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
-#
 # Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2020 StatiXOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 DEVICE_PATH := device/essential/mata
 
@@ -22,7 +21,6 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a73
-TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
@@ -32,65 +30,54 @@ TARGET_2ND_CPU_VARIANT := cortex-a73
 
 TARGET_BOOTLOADER_BOARD_NAME := Mata
 TARGET_BOARD_PLATFORM := msm8998
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno540
-TARGET_HAS_NO_SELECT_BUTTON := true
-
-TARGET_USES_64_BIT_BINDER := true
 
 TARGET_USES_AOSP := true
-# APEX image
+
+# APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
 # Audio
-BOARD_SUPPORTS_SOUND_TRIGGER_HAL := true
-BOARD_USES_ALSA_AUDIO := true
-DTS_CODEC_M_ := false
-MM_AUDIO_ENABLED_FTM := true
-MM_AUDIO_ENABLED_SAFX := true
 USE_CUSTOM_AUDIO_POLICY := 1
 USE_XML_AUDIO_POLICY_CONF := 1
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-QCOM_BT_USE_BTNV := true
 
 # Camera
 TARGET_USES_QTI_CAMERA_DEVICE := true
-USE_CAMERA_STUB := true
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # Display
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+TARGET_SCREEN_DENSITY := 480
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
 TARGET_USES_HWC2 := true
 TARGET_USES_GRALLOC1 := true
 TARGET_DISPLAY_SHIFT_HORIZONTAL := 64
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
-SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
-TARGET_HAS_HDR_DISPLAY := true
-
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
 
 # HIDL
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=mata user_debug=31 ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1
-BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
-BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += \
+    androidboot.configfs=true \
+    androidboot.hardware=mata \
+    androidboot.usbcontroller=a800000.dwc3 \
+    ehci-hcd.park=3 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    service_locator.enable=1 \
+    swiotlb=2048 \
+    user_debug=31
+
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
@@ -100,10 +87,6 @@ TARGET_KERNEL_SOURCE := kernel/essential/msm8998
 TARGET_KERNEL_CONFIG := electric_mata_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 11.0.1
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-
-# LLVM
-TARGET_USE_SDCLANG := true
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 536870912 # 500MB
@@ -118,6 +101,9 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 
+# Properties
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mata
 TARGET_RECOVERY_UI_MARGIN_WIDTH := 64
@@ -125,8 +111,9 @@ TARGET_RECOVERY_WIPE := $(DEVICE_PATH)/recovery.wipe
 
 # Root
 BOARD_ROOT_EXTRA_FOLDERS := persist
-BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
-BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
+BOARD_ROOT_EXTRA_SYMLINKS += \
+    /vendor/lib/dsp:/dsp \
+    /vendor/firmware_mnt:/firmware
 
 # SELinux
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
@@ -138,5 +125,5 @@ BOARD_VNDK_VERSION := current
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_TARGET_VNDK_VERSION := 29
 
-# Inherit from the proprietary version
+# Vendor
 include vendor/essential/mata/BoardConfigVendor.mk
