98222b6ef9bc579fa6b830664e57c571dde12563
Alexander Koskovich <zvnexus@outlook.com>
2020-06-05 02:23:33 +0000

    mata: Cleanup DT.
    Change-Id: Iceae379e336184c7fcdec86ff76b751027460873

--- overlay/EssentialFrameworks/Android.mk
+++ overlay/EssentialFrameworks/Android.mk
@@ -1,10 +1,10 @@
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2020 StatiXOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,5 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES += \
-    device/essential/mata/statix_mata.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_PACKAGE_NAME := EssentialFrameworks
+LOCAL_MODULE_OWNER := essential
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_SDK_VERSION := current
+include $(BUILD_RRO_PACKAGE)
