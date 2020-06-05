f05f398fdbbc2f8c056c06cdb232fbea47c3ba43
Alexander Koskovich <zvnexus@outlook.com>
2020-06-05 02:23:33 +0000

    mata: Cleanup DT.
    Change-Id: Iceae379e336184c7fcdec86ff76b751027460873

--- config.fs
+++ config.fs
@@ -1,50 +1,5 @@
-[AID_VENDOR_QTI_DIAG]
-value:2901
- 
 [system/product/vendor_overlay/*/bin/*]
 mode: 0755
 user: AID_ROOT
 group: AID_SHELL
 caps: 0
-
-[vendor/bin/cnd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
-
-[vendor/bin/imsdatadaemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[vendor/bin/imsrcsd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: WAKE_ALARM
-
-[vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
-[vendor/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[vendor/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[vendor/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
