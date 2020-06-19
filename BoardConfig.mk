ddfca28b2dea8d7560569ea53bb3d61c42bc948c
Anay Wadhera <awadhera@berkeley.edu>
2020-06-18 22:55:34 -0700

    mata: add recovery fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,6 +88,9 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Properties
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
+
 # SEPolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
