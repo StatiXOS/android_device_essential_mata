0661ec9125df9b4b4fd790d55a36cef692d6dfc8
Anay Wadhera <awadhera@berkeley.edu>
2020-09-11 17:17:50 -0700

    mata: statix

--- statix_mata.mk
+++ statix_mata.mk
@@ -1,6 +1,7 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -15,16 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Device
 $(call inherit-product, device/essential/mata/device.mk)
 
-# TWRP
-ifeq ($(WITH_TWRP),true)
-$(call inherit-product, device/essential/mata/twrp/twrp.mk)
-else
-TARGET_RECOVERY_FSTAB := device/essential/mata/rootdir/etc/fstab.mata
-endif
-
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := lineage_mata
+PRODUCT_NAME := statix_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
