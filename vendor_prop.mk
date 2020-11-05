435ec890879c84da6bb85cba0b9bca7611cae0d0
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2020-11-05 06:35:21 +0100

    mata: Drop all SF offset related props.
    For triple frame buffers 19db91f2cfc71b5cd924268b5c40bf329fdc6efd to work properly, these props
    need to be dropped to prevent any unneeded regressions.
    
    Change-Id: I058cef2c28af1d6512d88f7fc5e840c0aaedbd01

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -167,15 +167,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+    ro.surface_flinger.protected_contents=true
 
 # Time daemon
 PRODUCT_PROPERTY_OVERRIDES += \
