19863094c90cc512c4f89489f0c675d277316b3e
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-10-07 09:39:04 -0400

    mata: Build kernel with GCC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,8 +92,9 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/essential/msm8998
-TARGET_KERNEL_CONFIG := electric_mata_defconfig
-TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := mata_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
+KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
