9367d09967d08168119ca35b29350805897220f0
Anay Wadhera <awadhera@berkeley.edu>
2021-11-04 11:23:58 -0700

    mata: Build packages for updated qcrilhook and qti Bluetooth
    * This is a squashed changeset.

--- device.mk
+++ device.mk
@@ -95,6 +95,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
     libbt-vendor \
     libbthost_if \
     vendor.qti.hardware.btconfigstore@1.0 \
@@ -352,6 +353,12 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
