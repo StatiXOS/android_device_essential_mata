40230df9418d2c36a2b5ca3d089c1ac8e6242eb7
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-02-05 12:59:14 -0500

    mata: Refactor Statix makefile
    Change-Id: I6ad0315132a50993aee0986b8fc0ac4fd250754b

--- statix_mata.mk
+++ statix_mata.mk
@@ -1,11 +1,15 @@
-# Inherit some common AOSP stuff.
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+# Inherit some AOSP stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Vendor blobs
 $(call inherit-product, vendor/essential/mata/mata-vendor.mk)
